db62684c79969f87904f27585a22bf8abde7e969
GuillermoCochrane <guilleac81@gmail.com>
2025-01-08 17:15:53 -0300

    Agregado modelo Originacion

---
+++ src/database/models/Originacion.js
@@ -0,0 +1,77 @@
+module.exports = function(sequelize, DataTypes) {
+
+  let alias = 'Originacion';
+
+  let cols = {
+    id: {
+       // ID de la originación, clave primaria autoincremental
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+
+    fecha_de_observacion: {
+      // Fecha de la observación, obligatorio
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    
+    lugar: {
+      // Lugar de la observación, máximo 60 caracteres, obligatorio
+      type: DataTypes.STRING(60),
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1, 60],
+          msg: 'El lugar de la observación debe tener entre 1 y 60 caracteres.',
+        },
+        notEmpty: {
+          msg: 'El campo "lugar" no puede estar vacío.',
+        },
+      },
+    },
+
+    ente_inspector_id: {
+      // ID del ente inspector al que pertenece la observación, obligatorio
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+
+    origen_id: {
+      // ID del origen al que pertenece la observación, obligatorio
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+
+    observador_id: {
+      // ID del observador al que pertenece la observación, obligatorio
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+
+    sector_id: {
+      // ID del sector al que pertenece la observación, obligatorio
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+
+    estado_id: {
+      // ID del estado al que pertenece la observación, obligatorio
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+  };
+
+  let config = {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    deletedAt: false,
+    tableName: 'originaciones'
+  };
+
+  const Originacion = sequelize.define(alias, cols, config);
+
+  return Originacion;
+};
