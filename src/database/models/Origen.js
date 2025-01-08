b847cea45aa50afa510bb719849dd4aac2c1be59
GuillermoCochrane <guilleac81@gmail.com>
2025-01-08 14:12:27 -0300

    Agregado modelo Origen

---
+++ src/database/models/Origen.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, DataTypes) {
+
+  let alias = 'Origen';
+
+  let cols = {
+    id: {
+       // ID del origen, clave primaria autoincremental
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+
+    origen: {
+      // Nombre del origen, máximo 100 caracteres
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1, 100],
+          msg: 'El nombre del origen debe tener entre 1 y 100 caracteres.',
+        },
+        notEmpty: {
+          msg: 'El campo "origen" no puede estar vacío.',
+        },
+      },
+    },
+  };
+
+  let config = {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    deletedAt: false,
+    tableName: 'origenes'
+  };
+
+  const Origen = sequelize.define(alias, cols, config);
+
+  return Origen;
+};
