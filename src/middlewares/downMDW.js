c0fbd4a2d0c6e4a1190ae3eb8a8956e7960bb190
GuillermoCochrane <guilleac81@gmail.com>
2025-01-22 18:07:34 -0300

    Middleware para dehabilitar una ruta

---
+++ src/middlewares/downMDW.js
@@ -0,0 +1,11 @@
+//Middleware de ruta que redireciona la ruta a home, cuando se setea la variable en true
+function downMDW (req,res, next) {
+    let downRoute = true;
+    if(downRoute){
+        return res.redirect("/")
+    }
+    next();
+}
+
+module.exports = downMDW
+
