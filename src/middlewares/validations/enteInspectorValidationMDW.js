3c319a82cc09dc160dd2376fc97ab5c8081f289b
GuillermoCochrane <guilleac81@gmail.com>
2025-01-23 19:19:05 -0300

    Middleware para validar los datos del formulario de Ente Inspector

---
+++ src/middlewares/validations/enteInspectorValidationMDW.js
@@ -0,0 +1,9 @@
+const{body} = require("express-validator");
+const enteInspectorValidationMDW = [
+    body("ente_inspector")
+      .notEmpty().withMessage("Debe completar el campo Ente Inspector").bail()
+      .isLength({min: 2}).withMessage("El campo Ente Inspector no puede tener menos de 2 caracteres").bail()
+      .isLength({max: 100}).withMessage("El campo Ente Inspector no puede tener más de 100 caracteres")
+];
+
+module.exports = sectorValidationMDW;
