cb2067310f6e6cee4c85cda425ddfd4fb295b730
GuillermoCochrane <guilleac81@gmail.com>
2025-01-25 16:43:19 -0300

    Modificado id del formualrio, por uno más generico

--- public/js/dashboard/sectionhandler.js
+++ public/js/dashboard/sectionhandler.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () =>{
   let mainTitle = document.getElementById("mainTitle");
   let newTitle = document.getElementById("newTitle");
   let mainContent = document.getElementById("main-content");
-  let newContent = document.getElementById("new-content");
+  let dashboardForm = document.getElementById("dashboard-form");
   
   mainButton.addEventListener("click", () => toggleElement(1));
   newButton.addEventListener("click", () => toggleElement(2));
@@ -14,14 +14,14 @@ window.addEventListener("load", () =>{
           mainTitle.style.display = "block";
           newTitle.style.display = "none";
           mainContent.classList.remove("hidden");
-          newContent.classList.add("hidden");
+          dashboardForm.classList.add("hidden");
 					mainButton.classList.add("inactive");
 					newButton.classList.remove("inactive");
       } else if (buttonNumber === 2) {
           mainTitle.style.display = "none";
           newTitle.style.display = "block";
           mainContent.classList.add("hidden");
-          newContent.classList.remove("hidden");
+          dashboardForm.classList.remove("hidden");
 					mainButton.classList.remove("inactive");
           newButton.classList.add("inactive");
       }
