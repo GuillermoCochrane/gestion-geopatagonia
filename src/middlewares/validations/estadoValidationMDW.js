f3eb930d57963f0afb7f20700f9897df8302fb40
GuillermoCochrane <guilleac81@gmail.com>
2025-01-29 16:58:06 -0300

    Corregido error surgido por cambios en la DB

--- src/middlewares/validations/estadoValidationMDW.js
+++ src/middlewares/validations/estadoValidationMDW.js
@@ -1,9 +1,9 @@
 const{body} = require("express-validator");
 const estadoValidationMDW = [
-    body("nombre")
-      .notEmpty().withMessage("Debe completar el campo nombre").bail()
-      .isLength({min: 2}).withMessage("El campo nombre no puede tener menos de 2 caracteres").bail()
-      .isLength({max: 60}).withMessage("El campo nombre no puede tener más de 60 caracteres"),
+    body("estado")
+      .notEmpty().withMessage("Debe completar el campo estado").bail()
+      .isLength({min: 2}).withMessage("El campo estado no puede tener menos de 2 caracteres").bail()
+      .isLength({max: 60}).withMessage("El campo estado no puede tener más de 60 caracteres"),
     body("descripcion")
       .isLength({max: 300}).withMessage("El campo descripcion no puede tener más de 300 caracteres")
 ];
