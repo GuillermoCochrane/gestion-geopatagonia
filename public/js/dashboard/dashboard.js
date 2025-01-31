e87ea30127188301bcc401ae9faf22501b8ab91f
GuillermoCochrane <guilleac81@gmail.com>
2025-01-31 07:41:59 -0300

    Script para mostrar y ocultar sidebar, y modificaciones para que el mismo funcione

--- public/js/dashboard/dashboard.js
+++ public/js/dashboard/dashboard.js
@@ -1,3 +1,8 @@
 window.addEventListener("load", () =>{
-  console.log("script cargado");
+    const $menu = document.querySelector("#menu");
+    const $sidebar = document.querySelector("#sidebar");
+    $menu.addEventListener("click", () =>{
+        $sidebar.classList.toggle("hidden");
+        console.log($sidebar.classList);
+    })
 })
