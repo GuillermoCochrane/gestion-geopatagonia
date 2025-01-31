2532d374635c691e79a5444894dc6b7f854d1eac
GuillermoCochrane <guilleac81@gmail.com>
2025-01-31 06:48:23 -0300

    Script de validaciones de sector

---
+++ public/js/dashboard/validations/sectorValidation.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+    const $form = document.querySelector("#dashboard-form");
+    const $sector = document.querySelector("#sector");
+    const $btn = document.querySelector("#dashboard-form button");
+
+    const sectorValidation = () => {
+        requiredValidation($sector);
+        errors.sector ? null : minlengthValidation($sector,2);
+        errors.sector ? null : maxlengthValidation($sector,100);
+    };
+
+    $sector.addEventListener("input",() => sectorValidation($sector));
+    $sector.addEventListener("blur", () => sectorValidation($sector));
+
+    $btn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        sectorValidation($sector);
+        if (Object.keys(errors).length == 0) {
+            $form.submit();
+        }
+    });
+});
