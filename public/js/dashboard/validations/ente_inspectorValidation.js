a3b17fc7fd36f66468c96e1ec9367cfa5810b632
GuillermoCochrane <guilleac81@gmail.com>
2025-01-31 07:01:20 -0300

    Script de validaciones de ente inspector

---
+++ public/js/dashboard/validations/ente_inspectorValidation.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+    const $form = document.querySelector("#dashboard-form");
+    const $ente_inspector = document.querySelector("#ente_inspector");
+    const $btn = document.querySelector("#dashboard-form button");
+
+    const ente_inspectorValidation = () => {
+        requiredValidation($ente_inspector);
+        errors.ente_inspector ? null : minlengthValidation($ente_inspector,2);
+        errors.ente_inspector ? null : maxlengthValidation($ente_inspector,60);
+    };
+
+    $ente_inspector.addEventListener("input",() => ente_inspectorValidation($ente_inspector));
+    $ente_inspector.addEventListener("blur", () => ente_inspectorValidation($ente_inspector));
+
+    $btn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        ente_inspectorValidation($ente_inspector);
+        if (Object.keys(errors).length == 0) {
+            $form.submit();
+        }
+    });
+});
