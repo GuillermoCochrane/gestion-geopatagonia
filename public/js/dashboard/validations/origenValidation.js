7dce8db6a41b73348620d752ebc3b6f63526bdf0
GuillermoCochrane <guilleac81@gmail.com>
2025-01-31 06:44:25 -0300

    Script de validaciones de origen

---
+++ public/js/dashboard/validations/origenValidation.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", () => {
+    const $form = document.querySelector("#dashboard-form");
+    const $origen = document.querySelector("#origen");
+    const $btn = document.querySelector("#dashboard-form button");
+
+    console.log("cargo");
+    
+
+    const origenlValidation = () => {
+        requiredValidation($origen);
+        errors.origen ? null : minlengthValidation($origen,2);
+        errors.origen ? null : maxlengthValidation($origen,100);
+    };
+
+    $origen.addEventListener("input",() => origenlValidation($origen));
+    $origen.addEventListener("blur", () => origenlValidation($origen));
+
+    $btn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        origenlValidation($origen);
+        if (Object.keys(errors).length == 0) {
+            $form.submit();
+        }
+    });
+});
