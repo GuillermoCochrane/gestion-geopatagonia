55fe5c3e30f320dda4322696718ecf536146ca29
GuillermoCochrane <guilleac81@gmail.com>
2025-02-13 19:32:30 -0300

    Modificado script que abre y cierra la barra de navegación, para que también cambie el ícono del botón

--- public/js/dashboard/dashboard.js
+++ public/js/dashboard/dashboard.js
@@ -2,7 +2,13 @@ window.addEventListener("load", () =>{
     const $menu = document.querySelector("#menu");
     const $sidebar = document.querySelector("#sidebar");
     $menu.addEventListener("click", () =>{
+        if ($menu.classList.contains("fa-bars")) {
+            $menu.classList.remove("fa-bars");
+            $menu.classList.add("fa-xmark");
+        } else if ($menu.classList.contains("fa-xmark")) {
+            $menu.classList.remove("fa-xmark");
+            $menu.classList.add("fa-bars");
+        }
         $sidebar.classList.toggle("hidden");
-        console.log($sidebar.classList);
     })
 })
