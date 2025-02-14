5bcd7877cd83cb722f4cb50f9483dadb76012569
GuillermoCochrane <guilleac81@gmail.com>
2025-02-14 19:47:14 -0300

    Modificado index de originación para cargar la vista preliminar de la página

--- src/controllers/orginiacionController.js
+++ src/controllers/orginiacionController.js
@@ -1,10 +1,14 @@
 const { Estado, EnteInspector, Origen, Sector, Rol, Usuario } = require("../database/models");
-const dashboardUtilities = require("../utilities/dashboardUtilitites");
 const { validationResult } = require('express-validator');
 
 const originacionController = {
     index: async (req, res) => {
-        return res.send("Bienvenido a Originaciones")
+        return res.render("originacion/orginacion",
+            {
+                title: "Originaciones", 
+                styles: ["dashboard"],
+                pageScript: ["dashboard/dashboard"],
+            });
     },
 }
 
