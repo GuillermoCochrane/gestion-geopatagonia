07cddd2bfc2ea0f247f97588223dcf187e679f66
GuillermoCochrane <guilleac81@gmail.com>
2025-03-04 17:55:20 -0300

    Script modificado con nombres más génericos, para que sea reutilizable y ubicado en la carpeta correspondiente

--- public/js/sectionhandler.js
+++ public/js/sectionhandler.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () =>{
   let mainTitle = document.getElementById("mainTitle");
   let newTitle = document.getElementById("newTitle");
   let mainContent = document.getElementById("main-content");
-  let dashboardForm = document.getElementById("dashboard-form");
+  let newContent = document.getElementById("new-content");
   
   mainButton.addEventListener("click", () => toggleElement(1));
   newButton.addEventListener("click", () => toggleElement(2));
@@ -14,14 +14,14 @@ window.addEventListener("load", () =>{
           mainTitle.style.display = "block";
           newTitle.style.display = "none";
           mainContent.classList.remove("hidden");
-          dashboardForm.classList.add("hidden");
+          newContent.classList.add("hidden");
 					mainButton.classList.add("inactive");
 					newButton.classList.remove("inactive");
       } else if (buttonNumber === 2) {
           mainTitle.style.display = "none";
           newTitle.style.display = "block";
           mainContent.classList.add("hidden");
-          dashboardForm.classList.remove("hidden");
+          newContent.classList.remove("hidden");
 					mainButton.classList.remove("inactive");
           newButton.classList.add("inactive");
       }
