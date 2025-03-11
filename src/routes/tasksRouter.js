597a59a277dbc27c05e26a98a0267b3445e9be55
GuillermoCochrane <guilleac81@gmail.com>
2025-03-11 18:09:09 -0300

    Implementado Middleware de guardado de archivos para la creación de originaciones

--- src/routes/tasksRouter.js
+++ src/routes/tasksRouter.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const tasksController = require('../controllers/tasksController');
 
+//Middlewares
+
+// Multer
+const upload = require("../middlewares/multer/originacionMulterMDW");
+
 router.get('/', tasksController.index);
 router.get('/originacion', tasksController.originacion);
-router.post('/originacion/nueva', tasksController.nuevaOriginacion);
+router.post('/originacion/nueva', upload.single("adjunto"), tasksController.nuevaOriginacion);
 
 module.exports = router;
