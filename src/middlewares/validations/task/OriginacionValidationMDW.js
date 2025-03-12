3c02e7afa5edc156015c52513ff4b8f9778a51a0
GuillermoCochrane <guilleac81@gmail.com>
2025-03-12 18:22:21 -0300

    Middleware de validaciones de originaciones

---
+++ src/middlewares/validations/task/OriginacionValidationMDW.js
@@ -0,0 +1,26 @@
+const{body} = require("express-validator");
+const originacionValidationMDW = [
+    body("fecha_de_observacion")
+      .notEmpty().withMessage("Debe completar la fecha").bail()
+      .isDate().withMessage("La fecha debe ser válida").bail(),
+    body("lugar")
+      .notEmpty().withMessage("Debe completar el lugar").bail()
+      .isLength({min: 2}).withMessage("El lugar debe ser de al menos de 2 caracteres").bail()
+      .isLength({max: 60}).withMessage("El lugar no puede tener más de 60 caracteres"),
+    body("origen_id")
+      .notEmpty().withMessage("Debe seleccionar el origen").bail()
+      .isInt({ min: 1 }).withMessage("Debe seleccionar un origen válido"),
+    body("observador_id")
+      .notEmpty().withMessage("Debe seleccionar el observador").bail()
+      .isInt({ min: 1 }).withMessage("Debe seleccionar un observador válido"),
+    body("ente_insperctor_id")
+      .notEmpty().withMessage("Debe seleccionar el ente inspector").bail()
+      .isInt({ min: 1 }).withMessage("Debe seleccionar un ente inspector válido"),
+    body("sector_id")
+      .notEmpty().withMessage("Debe seleccionar el sector").bail()
+      .isInt({ min: 1 }).withMessage("Debe seleccionar un sector válido"),
+    body("adjunto")
+      .notEmpty().withMessage("Debe seleccionar el adjunto").bail(),
+];
+
+module.exports = originacionValidationMDW;
