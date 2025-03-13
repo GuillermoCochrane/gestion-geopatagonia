dde9e37339afa887cd87358ac41156180f5bb6e1
GuillermoCochrane <guilleac81@gmail.com>
2025-03-13 20:27:42 -0300

    Script modificado para que el modal aparezca abierto en caso de errores de validación del backend

--- public/js/task/modalManager.js
+++ public/js/task/modalManager.js
@@ -1,8 +1,18 @@
-window.addEventListener("load", () =>{
+window.addEventListener("load", () => {
   const modal = document.getElementById("modal");
   const modalCloser = document.getElementById("modal-closer");
   const modalOpener = document.getElementById("modal-opener");
 
+  // Abrir el modal con el botón
   modalOpener.addEventListener("click", () => modal.showModal());
+
+  // Cerrar el modal con el botón
   modalCloser.addEventListener("click", () => modal.close());
-})
+
+  // Verificar si hay errores de validación al cargar la página
+  const hasErrors = modal.dataset.hasErrors === "true"; // Leer el valor del atributo data-has-errors
+
+  if (hasErrors) {
+    modal.showModal(); // Abre el modal como un modal nativo
+  }
+});
