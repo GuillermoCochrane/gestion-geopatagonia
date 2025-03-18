a2c2eeaffda6891debf4c6e24d8cdbadfb4a41fa
GuillermoCochrane <guilleac81@gmail.com>
2025-03-18 17:31:25 -0300

    Agregada función fileExtentionValidation para validar extensiones de archivo permitidas

--- public/js/validations.js
+++ public/js/validations.js
@@ -2,7 +2,7 @@ let errors = {};
 
 const baseUrl = window.location.origin;
 
-// Funciones para manejar clases CSS
+//! Funciones para manejar clases CSS
 const inputError = (input) => {
   input.classList.remove("input-ok");
   input.classList.add("input-error");
@@ -13,14 +13,14 @@ const inputOK = (input) => {
   input.classList.add("input-ok");
 };
 
-// Función para convertir " _ " a espacios
+//! Función para convertir " _ " a espacios
 const underscoreToSpace = (string) => {
   return string.includes('_') 
           ? string.replace(/_/g, ' ') 
           : string;
 };
 
-// Función de utilidad para manejar errores
+//! Función de utilidad para manejar errores
 const handleValidation = (input, validation, errorMessage) => {
   const label = input.id;
   const errorElement = document.querySelector(`#error-${label}`);
@@ -36,6 +36,7 @@ const handleValidation = (input, validation, errorMessage) => {
   }
 };
 
+//! Función para dividir una fecha en  año, mes y día. 
 const splitDate = (date) =>{
     // Dividir la fecha en partes (año, mes, día)
     const partesFecha = date.split("-");
@@ -46,31 +47,37 @@ const splitDate = (date) =>{
     }
 }
 
-// Funciones de validación
+// ! Funciones de validación
+
+// * Validación de campos obligatorios
 const requiredValidation = (input) => {
   const validation = !validator.isEmpty(input.value);
   const errorMessage = `${underscoreToSpace(input.id)} es obligatorio`;
   handleValidation(input, validation, errorMessage)
 };
 
+// * Validación de longitud mínima
 const minlengthValidation = (input,min) => {
   const validation = validator.isLength(input.value, { min });
   const errorMessage = `${underscoreToSpace(input.id)} debe tener mínimo ${min} caracteres`;
   handleValidation(input, validation, errorMessage);
 };
 
+// * Validación de longitud máxima
 const maxlengthValidation = (input,max) => {
   const validation = validator.isLength(input.value, { max });
   const errorMessage = `${underscoreToSpace(input.id)} debe tener máximo ${max} caracteres`;
   handleValidation(input, validation, errorMessage);
 };
 
+// * Validación de formato de email
 const isEmailValidation = (input) => {
   const validation = validator.isEmail(input.value);
   const errorMessage = `${underscoreToSpace(input.id)} no es un email válido`;
   handleValidation(input, validation, errorMessage);
 };
 
+// * Validación de email único
 const uniqueValidation = async (input, id = null) => {
   let endpoint = `${baseUrl}/api/utilities/inUseEmail/${input.value}`;
   if (id) endpoint += `/${id}`;
@@ -81,14 +88,15 @@ const uniqueValidation = async (input, id = null) => {
   handleValidation(input, validation, errorMessage);
 };
 
+// * Validación de contraseña segura
 const strongValidation = (input) => {
   const validation = validator.isStrongPassword(input.value);
   const errorMessage = `${underscoreToSpace(input.id)} debe tener al menos una mayúscula, una minúscula, un número y un caracter especial`;
   handleValidation(input, validation, errorMessage);
 };
 
+// * Validación de fechas con formato valido (YYYY-MM-DD)
 const isDateValidation = (input) => {
-  // Validar que el valor sea una fecha válida en el formato YYYY-MM-DD
   const datePattern = /^\d{4}-\d{2}-\d{2}$/; // Expresión regular para YYYY-MM-DD
   const isValidFormat = datePattern.test(input.value); // Verificar el formato
 
@@ -107,9 +115,9 @@ const isDateValidation = (input) => {
   handleValidation(input, validation, errorMessage);
 };
 
+// * Validación que compruebe si la fecha ingresada no sea anterior a la fecha actual
 const notOlderValidation = (input) => {
   const {año, mes, dia} = splitDate(input.value);   // Dividir la fecha en partes (año, mes, día)
-  // Crear la fecha ingresada y la actual
   const fechaIngresada = new Date(año, mes, dia);
   const fechaActual = new Date();
   // Reiniciar la hora en ambas fechas para comparar solo la fecha
@@ -121,6 +129,17 @@ const notOlderValidation = (input) => {
 
   const errorMessage = `${underscoreToSpace(input.id)} no puede ser anterior a hoy`;
 
-  // Manejar la validación
+  handleValidation(input, validation, errorMessage);
+};
+
+// * Validación de extensiones de archivos permitidas
+const extentionValidation = (input, allowedExtensions) => {
+  const fileName = input.value; 
+  const fileExtention = fileName.split('.').pop()?.toLowerCase(); // Extrae la extensión y la convierte a minúsculas
+
+ // Verifica si la extensión está en la lista de permitidas
+  const validation = fileExtention && allowedExtensions.includes(fileExtention);
+
+  const errorMessage = `Solo se permiten archivos ${allowedExtensions.join(', ')}`;
   handleValidation(input, validation, errorMessage);
 };
