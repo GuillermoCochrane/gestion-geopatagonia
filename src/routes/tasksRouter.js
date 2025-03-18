513eed805eeab5b8d935fb94409f046aae34e506
GuillermoCochrane <guilleac81@gmail.com>
2025-03-18 18:19:34 -0300

    Modificada ruta de procesamiento del formulario

--- src/routes/tasksRouter.js
+++ src/routes/tasksRouter.js
@@ -14,6 +14,6 @@ const originacionValidation = require("../middlewares/validations/task/Originaci
 
 router.get('/', tasksController.index);
 router.get('/originacion', tasksController.originacion);
-router.post('/originacion/nueva', upload.single("adjunto"), originacionValidation, tasksController.nuevaOriginacion);
+router.post('/originacion/', upload.single("adjunto"), originacionValidation, tasksController.nuevaOriginacion);
 
 module.exports = router;
