719ebb69fff43b57b6596bfa7b9d87406f972c16
GuillermoCochrane <guilleac81@gmail.com>
2025-03-20 17:32:57 -0300

    Modificado singleOriginationData para dar formato a  la fecha de observación y agregar el script de manejo de subsecciones

--- src/utilities/taskUtilitites.js
+++ src/utilities/taskUtilitites.js
@@ -67,11 +67,13 @@ const taskUtilities = {
 
   originationPACData: async function(Originacion, Origen, Observador, EnteInspector, Sector, ObservacionPAC, AdjuntoOriginacion, formData, file){
     let data = this.headerData("Originaciones");
-    data.pageScript = [...data.pageScript, ...this.validationScripts]; // ? agregaremos luego validaciones de PACs
+    data.pageScript = [...data.pageScript, ...this.validationScripts, "sectionhandler"]; // ? agregaremos luego validaciones de PACs
+    console.log(data)
     data.dashboardHeader = this.dashboardHeader;
     try {
       const originacion = await this.createOriginacion(Originacion,AdjuntoOriginacion, formData, file); // Creación de la originación que devuelve un objeto con la originación y el adjunto
       const newOriginationData = await this.singleOriginationData(Originacion, Origen, Observador, EnteInspector, Sector, AdjuntoOriginacion, ObservacionPAC, originacion.originacion.id); // Obtener los datos de la originación
+      newOriginationData.fecha_de_observacion = utilities.formatDateDisplay(newOriginationData.fecha_de_observacion);
       //Datos adicionales para el el renderizado de la vista
       data.OriginationSaved = true;
       data.originacionData = newOriginationData;
