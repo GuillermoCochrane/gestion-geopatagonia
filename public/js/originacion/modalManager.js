34200218e1558482ff6b5be0c0e3243a578b001b
GuillermoCochrane <guilleac81@gmail.com>
2025-03-31 19:58:14 -0300

    Modificado scipt para que el muestre mensaje de advertencia si la originación no tiene observaciones / pacs

--- public/js/originacion/modalManager.js
+++ public/js/originacion/modalManager.js
@@ -2,12 +2,19 @@ window.addEventListener("load", () => {
   const modal = document.getElementById("modal");
   const modalCloser = document.getElementById("modal-closer");
   const modalOpener = document.getElementById("modal-opener");
+  const noObservacionesPACs = document.getElementById("noObservacionesPACs");
 
   // Abrir el modal con el botón
   modalOpener.addEventListener("click", () => modal.showModal());
 
   // Cerrar el modal con el botón
-  modalCloser.addEventListener("click", () => modal.close());
+  modalCloser.addEventListener("click", () => {
+    if (noObservacionesPACs != null){
+      const confirmarCierre = confirm("No hay observaciones registradas. Se eliminara la Originación. ¿Desea continuar?");
+      if (!confirmarCierre) return;
+    }
+    modal.close()
+  });
 
   // Verificar si hay errores de validación al cargar la página
   const hasErrors = modal.dataset.hasErrors === "true"; // Leer el valor del atributo data-has-errors
