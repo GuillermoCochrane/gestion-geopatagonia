97082792da8c2b62988e59087f1cd77583c579c4
GuillermoCochrane <guilleac81@gmail.com>
2025-04-08 19:38:49 -0300

    Agregarda funcionalidad para mostrar contraseña

--- public/js/dashboard/validations/usuarioValidation.js
+++ public/js/dashboard/validations/usuarioValidation.js
@@ -5,6 +5,23 @@ window.addEventListener("load", () => {
   const $password = document.querySelector("#password");
   const $id = document.querySelector("#id");
   const $btn = document.querySelector("#new-content button");
+	const $toggleIcon = document.querySelector("#toggle-icon");
+
+	let togglePassword = (input, icon) => {
+		if (input.type == "password"){
+				input.type = "text"
+				icon.classList.remove("fa-eye");
+				icon.classList.add("fa-eye-slash");
+		} else {
+				input.type = "password"
+				icon.classList.remove("fa-eye-slash");
+				icon.classList.add("fa-eye");
+		}
+	}
+
+	$toggleIcon.addEventListener("click", () => {
+		togglePassword($password, $toggleIcon);
+	});
 
   const nombreValidation = () => {
       requiredValidation($nombre);
