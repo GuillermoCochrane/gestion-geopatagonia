514244189b18f1a33876c5eb425d27f69fb5f8e5
GuillermoCochrane <guilleac81@gmail.com>
2025-04-11 20:46:57 -0300

    Agregada ruta de logout

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,10 @@ const mainController = {
     index: (req, res) => {
         res.redirect("/dashboard");
     },
+
+    logout: (req, res) => {
+        res.redirect("/dashboard");
+    }
 };
 
 module.exports = mainController;
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mainController = require("../controllers/mainController");
 
 router.get("/", mainController.index);
+router.get("/logout", mainController.logout);
 
 module.exports = router;
