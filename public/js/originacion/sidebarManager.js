8c608213abde131ccc80ca76b71f462d8cba982c
GuillermoCochrane <guilleac81@gmail.com>
2025-04-14 20:34:37 -0300

    Script modificado para cambiar el ícono del botón del menú del sidebar

--- public/js/originacion/sidebarManager.js
+++ public/js/originacion/sidebarManager.js
@@ -1,9 +1,21 @@
 window.addEventListener("load", () => {
   const sidebar = document.querySelector("#sidebar");
   const toggleBtn = document.querySelector("#toggle");
+  const toggleIcon = document.querySelector("#toggle-icon");
+
+  const styleManager = function(){
+    if (toggleIcon.classList.contains("fa-bars")) {
+      toggleIcon.classList.add("fa-xmark");
+      toggleIcon.classList.remove("fa-bars");
+    } else {
+      toggleIcon.classList.add("fa-bars");
+      toggleIcon.classList.remove("fa-xmark");
+    }
+  }
 
   toggleBtn.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
+    styleManager();
   });
 
 })
