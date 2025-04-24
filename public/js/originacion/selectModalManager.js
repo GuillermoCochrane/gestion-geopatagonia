c28197c5e4ee071f6d38e7e3f3a678aa3a391350
GuillermoCochrane <guilleac81@gmail.com>
2025-04-24 20:25:56 -0300

    Implementadas validaciones y funcionalidad de redirección, y corregidos errores de diseño de la plantilla

--- public/js/originacion/selectModalManager.js
+++ public/js/originacion/selectModalManager.js
@@ -2,8 +2,10 @@ window.addEventListener("load", () => {
   const $originacionSelectModal = document.querySelector("#select-originacion-modal");
   const $originationSelectorOpener = document.querySelector("#origination-selector-opener");
   const $originacionClose = document.querySelector("#originacion-close");
-  const $originacionSelect = document.querySelector("#originacion-select");
+  const $originacionSelectBtn = document.querySelector("#originacion-select");
+  const $originacionID = document.querySelector("#originacion_id");
   const $originacionForm = document.querySelector("#originacion-select-form");
+
   // Abrir el modal de selección de originación
   $originationSelectorOpener.addEventListener("click", () => {
     $originacionSelectModal.showModal();
@@ -12,5 +14,24 @@ window.addEventListener("load", () => {
   //Cerrar el modal de selección de originación
   $originacionClose.addEventListener("click", () => {
     $originacionSelectModal.close();
+  });
+
+  
+  const originacionValidation = () => {
+    requiredValidation($originacionID);
+  };
+
+  // Listeners 
+  $originacionID && $originacionID.addEventListener("input", originacionValidation);
+  $originacionID && $originacionID.addEventListener("blur", originacionValidation);
+
+  $originacionSelectBtn && $originacionSelectBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    originacionValidation();
+    if (Object.keys(errors).length == 0) {
+      // Redireccionar al detalle de la originación seleccionada
+      const selectedValue = $originacionID.value;
+      window.location.href = `${baseUrl}/originacion/${selectedValue}`;
+    } 
   });
 });
