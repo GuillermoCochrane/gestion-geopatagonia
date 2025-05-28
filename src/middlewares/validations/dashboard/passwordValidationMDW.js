2877dd3a9419ce0253a782e7d4afd097c542cea2
GuillermoCochrane <guilleac81@gmail.com>
2025-05-28 16:00:40 -0300

    Middleware modificado, para agregar comprobación de contraseña

--- src/middlewares/validations/dashboard/passwordValidationMDW.js
+++ src/middlewares/validations/dashboard/passwordValidationMDW.js
@@ -4,6 +4,13 @@ const passwordValidationMDW = [
       .notEmpty().withMessage("Debe completar la contraseña").bail()
       .isLength({min: 8}).withMessage("La contraseña debe tener 8 caracteres como mínimo ").bail()
       .isStrongPassword().withMessage("La contraseña debe tener al menos una mayúscula, una minúscula, un número y un caracter especial"),
+  body("check")
+      .custom((value, {req}) => {
+          if (value !== req.body.password) {
+              throw new Error("Las contraseñas no coinciden");
+          }
+          return true;
+      })
 ];
 
 module.exports = passwordValidationMDW;
