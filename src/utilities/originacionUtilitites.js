a2ef2213d6d9c8ee94493d04f1450755f5a9ed3b
GuillermoCochrane <guilleac81@gmail.com>
2025-07-09 18:14:34 -0300

    agregado método de creación de notificaciones de originaciones

--- src/utilities/originacionUtilitites.js
+++ src/utilities/originacionUtilitites.js
@@ -617,8 +617,19 @@ const originacionUtilitites = {
   },
 
   // Envia un correo electrónico a un usuario con notificacion de la originacion
-  sendNotification: async function(id, host, protocol){
-    
+  originacionNotification: async function(id){
+    try{
+      const origination = await Originacion.findByPk(id);
+      const user = await Usuario.findByPk(origination.observador_id);
+      const date = utilities.formatDateDisplay(origination.fecha_de_observacion);
+      const description = origination.lugar;
+      const data = mailutilities.orginacionNotification(user.nombre, date, description, origination.id);
+      await mailutilities.sendMail(user.email, data.subject, data.text); // podemos agrerarle el HTML y el mail de respaldo
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   },
 }
 
