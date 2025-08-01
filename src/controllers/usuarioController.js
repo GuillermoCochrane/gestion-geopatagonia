84e36fccd18b0b5eadce6d2ac3b359fe3f45967d
GuillermoCochrane <guilleac81@gmail.com>
2025-07-31 22:11:48 -0300

    Agregados rol y nombre a session

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -16,9 +16,11 @@ const usuarioController = {
     processLogin: async function (req, res) {
         let errors = validationResult(req);
         if (errors.isEmpty()){
-            const user = req.userFromDB;
-            if (user) {
-                req.session.user = utilities.encrypt(user.id);
+            const { id, rol } = userUtilities.encryptedSessionData(req.userFromDB);
+            if (req.userFromDB) {
+                req.session.user = id;
+                req.session.rol = rol;
+                req.session.nombre = req.userFromDB.nombre;
                 // Si se marca el checkbox de recordarme, se crea un cookie con el id del usuario que dura 1 hora
                 if(req.body.rememberMe) res.cookie("user", req.session.user, {maxAge: (1000*60)*60} ) //(1000*60 = 1000ms * 60s = 1 min) * 60min = 1h
                 return res.redirect("/usuario/logged");
