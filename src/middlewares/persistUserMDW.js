9a5a13e6b5f26975342f44fa7d1705d4ab06f9b9
GuillermoCochrane <guilleac81@gmail.com>
2025-07-31 22:04:34 -0300

    Middleware modificado para agregar los datos adicionales de session

--- src/middlewares/persistUserMDW.js
+++ src/middlewares/persistUserMDW.js
@@ -1,11 +1,16 @@
 //Middleware global que persiste la sesion del usuario, si hay cookie y no sesión
-function persistUserMDW(req, res, next) {
+const usuarioUtilities = require("../utilities/usuarioUtilities");
+
+async function persistUserMDW(req, res, next) {
     // Toma el valor falsy o trusty de la sesión
     res.locals.isLogged = Boolean(req.session.user);
 
     // Si hay cookie y no sesión, se guarda el valor de la cookie en la sesión y se establece como logged
     if (req.cookies.user && !req.session.user) { 
-        req.session.user = req.cookies.user; 
+        const data = await usuarioUtilities.encryptedRol(req.cookies.user);
+        req.session.user = req.cookies.user;
+        req.session.rol = data.rol;
+        req.session.nombre = data.nombre;
         res.locals.isLogged = true;
     }
     next();
