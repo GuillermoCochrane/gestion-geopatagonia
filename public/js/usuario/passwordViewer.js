198c4ca06f46d43b6ce4ad73bb068bddcb7812d9
GuillermoCochrane <guilleac81@gmail.com>
2025-08-05 02:56:24 -0300

    Script modficiado, para incluir toggle de repetir contraseña

--- public/js/usuario/passwordViewer.js
+++ public/js/usuario/passwordViewer.js
@@ -2,9 +2,15 @@ window.addEventListener("load", function() {
 
   const $password = document.querySelector("#password");
 	const $toggleIcon = document.querySelector("#toggle-icon");
+  const $repeatPassword = document.querySelector("#repeatPassword");
+  const $repeatToggleIcon = document.querySelector("#repeat-toggle-icon");
 
   $toggleIcon && $toggleIcon.addEventListener("click", () => {
     togglePassword($password, $toggleIcon);
+  });
+
+  $repeatToggleIcon && $repeatToggleIcon.addEventListener("click", () => {
+    togglePassword($repeatPassword, $repeatToggleIcon);
   });
 
 });
