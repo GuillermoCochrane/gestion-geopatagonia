6a215b5812d1d72d16bfbdb8d765f8e375824bc4
GuillermoCochrane <guilleac81@gmail.com>
2025-08-05 03:14:51 -0300

    Componente modificado, para adaptarlo a middlware de validación de password existente, y script adaptado por estos cambios

--- public/js/usuario/passwordViewer.js
+++ public/js/usuario/passwordViewer.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
 
   const $password = document.querySelector("#password");
 	const $toggleIcon = document.querySelector("#toggle-icon");
-  const $repeatPassword = document.querySelector("#repeatPassword");
+  const $repeatPassword = document.querySelector("#check");
   const $repeatToggleIcon = document.querySelector("#repeat-toggle-icon");
 
   $toggleIcon && $toggleIcon.addEventListener("click", () => {
