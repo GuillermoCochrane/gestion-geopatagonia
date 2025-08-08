108839f34955ea6a46128b5d5f87ebb537efc527
GuillermoCochrane <guilleac81@gmail.com>
2025-08-08 20:26:07 -0300

    Corregido error en Middleware

--- src/middlewares/access/guestMDW.js
+++ src/middlewares/access/guestMDW.js
@@ -1,8 +1,8 @@
 //Middleware de ruta que redireciona al login , cuando un usuario no está logeado
 function guestMDW (req,res, next) {
     const isDevMode = process.env.DISABLE_AUTH === "true"; // Variable de entorno para desactivar el middleware en desarrollo (eliminar en la version final)
-    if(!Boolean(req.session.user) && !isDevMode){
-        return res.redirect("/usuario/login")
+    if(Boolean(req.session.user) && !isDevMode){
+        return res.redirect("/usuario")
     }
     next();
 }
