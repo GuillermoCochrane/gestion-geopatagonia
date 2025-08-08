d1521ee6ef53391e1d8c4bc30fadf0eb6d2bfa6f
GuillermoCochrane <guilleac81@gmail.com>
2025-08-08 19:11:23 -0300

    Middlewares de acceso modificados para el uso de variables de entorno

--- src/middlewares/access/guestMDW.js
+++ src/middlewares/access/guestMDW.js
@@ -1,7 +1,7 @@
 //Middleware de ruta que redireciona al login , cuando un usuario no está logeado
 function guestMDW (req,res, next) {
-    const working = true // para desactivar el middleware en desarrollo
-    if(!Boolean(req.session.user) && working){
+    const isDevMode = process.env.DISABLE_AUTH === "true"; // Variable de entorno para desactivar el middleware en desarrollo (eliminar en la version final)
+    if(!Boolean(req.session.user) && !isDevMode){
         return res.redirect("/usuario/login")
     }
     next();
--- src/middlewares/access/loggedMDW.js
+++ src/middlewares/access/loggedMDW.js
@@ -1,7 +1,7 @@
 //Middleware de ruta que redireciona a usuario, cuando un usuario está logeado
 function loggedMDW (req,res, next) {
-    const working = true // para desactivar el middleware en desarrollo
-    if(Boolean(req.session.user) && working){
+    const isDevMode = process.env.DISABLE_AUTH === "true"; // Variable de entorno para desactivar el middleware en desarrollo (eliminar en la version final)
+    if(Boolean(req.session.user) && !isDevMode){
         return res.redirect("/usuario")
     }
     next();
