0b474f53219ea4cefe42f6a8d04404bf364d54b6
GuillermoCochrane <guilleac81@gmail.com>
2025-08-10 01:11:10 -0300

    Implementado sistema de permisos para la ruta de usuario

--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -13,19 +13,21 @@ const passwordValidationMDW = require("../middlewares/validations/dashboard/pass
 //? Acceso
 const tokenAccessMDW = require("../middlewares/access/usuario/tokenAccessMDW");
 const validTokenAccessMDW = require("../middlewares/access/usuario/validTokenAccessMDW");
+const loggeMDW = require("../middlewares/access/loggedMDW");
+const guestMDW = require("../middlewares/access/guestMDW");
 
 
 //* Rutas
-router.get('/', usuariosController.index);
-router.get('/login', usuariosController.login);
-router.post('/login', loginValidations, usuariosController.processLogin);
+router.get('/', loggeMDW, usuariosController.index);
+router.get('/login', guestMDW, usuariosController.login);
+router.post('/login', guestMDW, loginValidations, usuariosController.processLogin);
 router.get('/logged', usuariosController.logged);
-router.post('/logout', usuariosController.logout);
-router.get('/recovery', usuariosController.recovery);
-router.post('/recovery', recoveryValidations, usuariosController.processRecovery);
-router.get('/recovery/:token', tokenAccessMDW, usuariosController.tokenValidation);
-router.get('/validate', tokenAccessMDW, usuariosController.validateToken);
-router.post('/validate', tokenAccessMDW, tokenValidations, usuariosController.tokenValidation);
-router.get('/newPassword', tokenAccessMDW, validTokenAccessMDW, usuariosController.newPassword);
-router.post('/newPassword', tokenAccessMDW, validTokenAccessMDW, passwordValidationMDW, usuariosController.setNewPassword);
+router.post('/logout', loggeMDW, usuariosController.logout);
+router.get('/recovery', guestMDW, usuariosController.recovery);
+router.post('/recovery', guestMDW, recoveryValidations, usuariosController.processRecovery);
+router.get('/recovery/:token', guestMDW, tokenAccessMDW, usuariosController.tokenValidation);
+router.get('/validate', guestMDW, tokenAccessMDW, usuariosController.validateToken);
+router.post('/validate', guestMDW, tokenAccessMDW, tokenValidations, usuariosController.tokenValidation);
+router.get('/newPassword', guestMDW, tokenAccessMDW, validTokenAccessMDW, usuariosController.newPassword);
+router.post('/newPassword', guestMDW, tokenAccessMDW, validTokenAccessMDW, passwordValidationMDW, usuariosController.setNewPassword);
 module.exports = router;
